992b639d79f78ee4abdfec12e5d704855e960cda
senyehor <e.sencha2002@gmail.com>
2022-05-15 14:10:52 +0300

    added AGGREGATOR_SCRIPTS_PATH env variable

--- __main__.py
+++ __main__.py
@@ -1,16 +1,19 @@
+import os
+
 from aggregator.logger import logger
 from aggregator.aggregation_info import AggregationInfoJSONProcessor
 from aggregator.controllers import AggregationController, BoxListenerController
 
 
 def main():
+    scripts_path = os.getenv("AGGREGATOR_SCRIPTS_PATH")
     controller = AggregationController(
         aggregation_info_file_name="aggregation_info",
-        run_aggregation_script_name="./scripts/run_aggregator",
+        run_aggregation_script_name=f"{scripts_path}/run_aggregator",
         aggregation_info_processor=AggregationInfoJSONProcessor(),
         packet_listener_controller=BoxListenerController(
-            "./scripts/stop_box_listener",
-            "./scripts/resume_box_listener"
+            f"{scripts_path}/stop_box_listener",
+            f"{scripts_path}/resume_box_listener"
         )
     )
     logger.info("starting aggregation")
