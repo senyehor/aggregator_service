bb763aab1aacc7103631ee7f2ebe04facd486aa9
senyehor <e.sencha2002@gmail.com>
2022-05-15 14:04:04 +0300

    changed scripts paths to relative

--- __main__.py
+++ __main__.py
@@ -6,11 +6,11 @@ from aggregator.controllers import AggregationController, BoxListenerController
 def main():
     controller = AggregationController(
         aggregation_info_file_name="aggregation_info",
-        run_aggregation_script_name="scripts/run_aggregator",
+        run_aggregation_script_name="./scripts/run_aggregator",
         aggregation_info_processor=AggregationInfoJSONProcessor(),
         packet_listener_controller=BoxListenerController(
-            "scripts/stop_box_listener",
-            "scripts/resume_box_listener"
+            "./scripts/stop_box_listener",
+            "./scripts/resume_box_listener"
         )
     )
     logger.info("starting aggregation")
